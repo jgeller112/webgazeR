9f73fa158efae33bd5f0c8cfa4b0c5d804953e7c
Jason Geller <jasongeller@mdjgps405mcgu.bc.edu>
2024-12-09 15:52:37 -0500

    added none to downsample agg

--- R/downsample_gaze.R
+++ R/downsample_gaze.R
@@ -1,27 +1,27 @@
 #' Downsample gaze data
 #'
-#' This function will combine gaze samples into time bins.
+#' This function combines gaze samples into time bins and optionally aggregates the data.
 #' @param dataframe DataFrame containing gaze data.
 #' @param bin.length Length of time bins (in milliseconds).
 #' @param timevar Column name representing time.
-#' @param aggvars Vector of variable names to group by for aggregation.
-#' @return Downsampled DataFrame.
+#' @param aggvars Vector of variable names to group by for aggregation. Use "none" to skip aggregation.
+#' @return DataFrame with time bins added and optionally aggregated data.
 #' @export
 downsample_gaze <- function(dataframe, bin.length = 50, timevar = "time",
-                            aggvars = c("subject", "condition", "target", "trial", "object", "timebins")) {
-
-  # Create time bins
+                            aggvars = c("subject", "condition", "target", "trial", "object", "time_bin")) {
+  # Add time bins
   dataframe <- dataframe %>%
-    mutate(timebins = round(.data[[timevar]] / bin.length) * bin.length)
+    mutate(time_bin = floor(.data[[timevar]] / bin.length) * bin.length)
 
-  # Downsample the gaze data
-  if (length(aggvars) > 0) {
-    downsample <- dataframe %>%
-      group_by(across(all_of(aggvars))) %>%
-      summarize(Fix = mean(Looks))
-  } else {
-    downsample <- dataframe
+  # Skip aggregation if aggvars = "none"
+  if (aggvars == "none") {
+    return(dataframe)
   }
+
+  # Perform aggregation if aggvars is specified
+  downsample <- dataframe %>%
+    group_by(across(all_of(aggvars))) %>%
+    summarize(Fix = mean(Looks, na.rm = TRUE), .groups = "drop")
 
   return(downsample)
 }
