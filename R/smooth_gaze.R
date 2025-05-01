4d35a83be6100249996560cdfd4d6e5c9fda9faa
Jason Geller <jasongeller@mdjgps405mcgu.bc.edu>
2025-05-01 10:59:14 -0400

    add smooth-gaze

--- R/smooth_gaze.R
+++ R/smooth_gaze.R
@@ -1,15 +1,18 @@
-#' Apply a moving average smoothing function to gaze data (X and Y). This is generally reccomended after up-sampling the data.
+#' Apply a moving average smoothing function to gaze data (X and Y).
+#' This is generally recommended after up-sampling the data.
 #'
 #' @param x A data frame containing gaze data.
 #' @param n The window size (in samples) for the moving average.
 #' @param x_col The name of the X gaze column (as string).
 #' @param y_col The name of the Y gaze column (as string).
 #' @param trial_col The name of the trial column used for grouping (default = "Trial").
+#' @param subject_col The name of the subject column used for grouping (default = "Subject").
 #'
 #' @return A tibble with smoothed gaze X and Y columns (replacing originals).
 #' @export
 #'
-smooth_gaze <- function(x, n = 5, x_col = "Gaze_X", y_col = "Gaze_Y", trial_col = "Trial") {
+smooth_gaze <- function(x, n = 5, x_col = "Gaze_X", y_col = "Gaze_Y",
+                        trial_col = "Trial", subject_col = "Subject") {
   x <- dplyr::as_tibble(x)
 
   gaze_smooth <- function(df, gaze_col) {
@@ -28,7 +31,7 @@ smooth_gaze <- function(x, n = 5, x_col = "Gaze_X", y_col = "Gaze_Y", trial_col 
   }
 
   x <- x %>%
-    dplyr::group_by(.data[[trial_col]]) %>%
+    dplyr::group_by(.data[[subject_col]], .data[[trial_col]]) %>%
     gaze_smooth(x_col) %>%
     gaze_smooth(y_col) %>%
     dplyr::ungroup()
